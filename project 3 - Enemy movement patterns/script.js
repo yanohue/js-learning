a0d3b4f929e497b8c39cf64da7e9ccf8581419df
yanohue <yanohue@gmail.com>
2023-06-17 02:42:14 -0300

    Project 3 - first enemy pattern animations

---
+++ project 3 - Enemy movement patterns/script.js
@@ -0,0 +1,56 @@
+/** @type {HTMLCanvasElement} */
+const canvas = document.getElementById('canvas1');
+const ctx = canvas.getContext('2d');
+const CANVAS_WIDTH = canvas.width = 500;
+const CANVAS_HEIGTH = canvas.height = 1000;
+const numberOfEnemies = 10;
+const enemiesArray = [];
+
+let gameFrame = 0
+
+class Enemy {
+    constructor(){
+        this.image = new Image();
+        this.image.src = 'enemy1.png';
+        //this.speed = Math.random() * 4 - 2;
+        this.spriteWidth = 293;
+        this.spriteHeight = 155;
+        this.width = this.spriteWidth / 2.5;
+        this.height = this.spriteHeight / 2.5;
+        this.x = Math.random() * (canvas.width - this.width);
+        this.y = Math.random() * (canvas.height - this.height);
+        this.frame = 0;
+        this.flapSpeed = Math.floor(Math.random()* 3 + 1);
+    }
+    update() {
+        this.x += Math.random() * 5 - 2.5;
+        this.y += Math.random() * 5 - 2.5;
+        // animate sprite
+        if(gameFrame % this.flapSpeed === 0) {
+            this.frame > 4 ? this.frame = 0 : this.frame++; 
+        }
+    }
+    draw() {
+        ctx.drawImage(this.image, this.frame * this.spriteWidth, 0, this.spriteWidth, this.spriteHeight, this.x, this.y, this.width, this.height);
+    }
+};
+
+for (let i = 0; i < numberOfEnemies; i++) {
+    enemiesArray.push(new Enemy());
+}
+
+function animate() {
+    ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGTH);
+    enemiesArray.forEach(enemy => {
+        enemy.update();
+        enemy.draw();
+    });
+    gameFrame++;
+    requestAnimationFrame(animate);
+}
+animate();
+
+//enemy1 w=293 h=155
+//enemy2 w=266 h=188
+//enemy3 w=218 h=177
+//enemy4 w=319 h=212

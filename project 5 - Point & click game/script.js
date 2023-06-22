4992dd476a81c3862f0d9ed57492796569ea98a4
yanohue <yanohue@gmail.com>
2023-06-22 18:59:34 -0300

    Small fixes

--- project 5 - Point & click game/script.js
+++ project 5 - Point & click game/script.js
@@ -38,32 +38,40 @@ class Raven {
     update(deltaTime) {
         this.x -= this.directionX;
         this.y += this.directionY;
+
         if(this.x < 0 - this.width) this.markedForDeletion = true;
+
         this.timeSinceFlap += deltaTime;
+
         if(this.timeSinceFlap > this.flapInterval) {    
             if(this.frame > this.maxFrame) this.frame = 0;
             else this.frame++;
             this.timeSinceFlap = 0;
-        }
-    }
+        };
+    };
     draw() {
         ctx.strokeRect(this.x, this.y, this.width, this.height);
         ctx.drawImage(this.image, this.frame * this.spriteWidth, 0, this.spriteWidth, this.spriteHeight, this.x, this.y, this.width, this.height,)
-    }
+    };
 }
 
 function animate(timestamp) {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
+
     let deltaTime = timestamp - lastTime;
     lastTime = timestamp;
+
     timeToNextRaven += deltaTime;
+
     if(timeToNextRaven > ravenInterval) {
         ravens.push(new Raven());
         timeToNextRaven = 0;
-    }
+    };
+
     [...ravens].forEach(object => object.update(deltaTime));
     [...ravens].forEach(object => object.draw());
     ravens = ravens.filter(object => !object.markedForDeletion);
+
     requestAnimationFrame(animate);
 }
 animate(0);

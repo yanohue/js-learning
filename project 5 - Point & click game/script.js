f35a280f930d7b9d1a9e1d686d93795dc310cb29
Yano <108890917+yanohue@users.noreply.github.com>
2023-08-15 02:39:33 -0300

    Added explosion animation and animation & gameOver

--- project 5 - Point & click game/script.js
+++ project 5 - Point & click game/script.js
@@ -8,8 +8,10 @@ const collisionCtx = collisionCanvas.getContext('2d');
 collisionCanvas.width = window.innerWidth;
 collisionCanvas.height = window.innerHeight;
 
+ctx.font = '50px Impact';
+
 let score = 0;
-ctx.font = '50px Arial';
+let gameOver = false;
 
 let timeToNextRaven = 0;
 let ravenInterval = 500;
@@ -68,11 +70,60 @@ class Raven {
             }
             this.timeSinceFlap = 0;
         }
+        if(this.x < 0 - this.width) {
+            gameOver = true;
+        }
     }
     draw() {
         collisionCtx.fillStyle = this.color;
         collisionCtx.fillRect(this.x, this.y, this.width, this.height);
-        ctx.drawImage(this.image, this.frame * this.spriteWidth, 0, this.spriteWidth, this.spriteHeight, this.x, this.y, this.width, this.height,)
+        ctx.drawImage(this.image, this.frame * this.spriteWidth, 0, this.spriteWidth, this.spriteHeight, this.x, this.y, this.width, this.height)
+    }
+}
+
+let explosions = [];
+
+class Explosion {
+    constructor(x, y, size) {
+        this.image = new Image();
+        this.image.src = 'boom.png';
+
+        this.spriteWidth = 200;
+        this.spriteHeight = 179;
+
+        this.size = size;
+
+        this.x = x;
+        this.y = y;
+
+        this.frame = 0;
+
+        this.sound = new Audio();
+        this.sound.src = 'boom.wav';
+        this.sound.volume = 0.1;
+
+        this.timeSinceLastFrame = 0;
+        this.frameInterval = 96;
+
+        this.markedForDeletion = false;
+    }
+    update(deltaTime) {
+        if(this.frame === 0) {
+            this.sound.play();
+        }
+
+        this.timeSinceLastFrame += deltaTime;
+
+        if(this.timeSinceLastFrame > this.frameInterval) {
+            this.frame++;
+            this.timeSinceLastFrame = 0;
+            if(this.frame > 5) {
+                this.markedForDeletion = true;
+            }
+        }
+    }
+    draw() {
+        ctx.drawImage(this.image, this.frame * this.spriteWidth, 0, this.spriteWidth, this.spriteHeight, this.x, this.y - this.size / 4, this.size, this.size);
     }
 }
 
@@ -83,6 +134,18 @@ function drawScore() {
     ctx.fillText('Score: ' + score, 53, 78);
 }
 
+function drawGameOver() {
+    ctx.textAlign = 'center';
+
+    ctx.fillStyle = 'black';
+    ctx.fillText('GAME OVER', canvas.width / 2, canvas.height / 2);
+    ctx.fillStyle = 'red';
+    ctx.fillText('GAME OVER', canvas.width / 2 + 3, canvas.height / 2 + 3);
+
+    ctx.fillStyle = 'black';
+    ctx.fillText('my score: ' + score, canvas.width / 2, canvas.height / 2 + 100);
+}
+
 window.addEventListener('click', function(e) {
     const detectPixelColor = collisionCtx.getImageData(e.x, e.y, 1, 1);
     console.log(detectPixelColor);
@@ -91,6 +154,7 @@ window.addEventListener('click', function(e) {
         if(object.randomColors[0] === pc[0] && object.randomColors[1] === pc[1] && object.randomColors[2] === pc[2]) {
             object.markedForDeletion = true;
             score++;
+            explosions.push(new Explosion(object.x, object.y, object.width));
         }
     });
 });
@@ -112,9 +176,14 @@ function animate(timestamp) {
         });
     }
     drawScore();
-    [...ravens].forEach(object => object.update(deltaTime));
-    [...ravens].forEach(object => object.draw());
+    [...ravens, ...explosions].forEach(object => object.update(deltaTime));
+    [...ravens, ...explosions].forEach(object => object.draw());
     ravens = ravens.filter(object => !object.markedForDeletion);
-    requestAnimationFrame(animate);
+    explosions = explosions.filter(object => !object.markedForDeletion);
+    if(!gameOver) {
+        requestAnimationFrame(animate);
+    } else {
+        drawGameOver();
+    }
 }
 animate(0);

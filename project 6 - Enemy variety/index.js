b9543540684eb707f5c10b1e6e664802e6b0afac
yanohue <108890917+yanohue@users.noreply.github.com>
2024-06-15 17:38:49 -0300

    Added spider

--- project 6 - Enemy variety/index.js
+++ project 6 - Enemy variety/index.js
@@ -10,11 +10,12 @@ class Game {
         this.height = height;
 
         this.enemies = [];
-        this.enemyInterval = 1000;
+        this.enemyInterval = 500;
         this.enemyTimer = 0;
 
-        this.enemyTypes = ['worm', 'ghost']; 
+        this.enemyTypes = ['worm', 'ghost', 'spider'];
     }
+
     update(deltaTime) {
         this.enemies = this.enemies.filter(object => !object.markedForDeletion);
 
@@ -25,20 +26,24 @@ class Game {
         } else {
             this.enemyTimer += deltaTime;
         }
-            
+
         this.enemies.forEach(object => object.update(deltaTime));
     }
+
     draw() {
         this.enemies.forEach(object => object.draw(this.ctx));
     }
+
     #addNewEnemy() {
         const randomEnemy = this.enemyTypes[Math.floor(Math.random() * this.enemyTypes.length)];
-        
+
         if(randomEnemy == 'worm') {
             this.enemies.push(new Worm(this));
         }
         else if(randomEnemy == 'ghost') {
             this.enemies.push(new Ghost(this));
+        } else if(randomEnemy === 'spider') {
+            this.enemies.push(new Spider(this));
         }
 
         /* enemies at the top appear behind enemies at the bottom
@@ -52,18 +57,36 @@ class Game {
 class Enemy {
     constructor(game) {
         this.game = game;
-                
         this.markedForDeletion = false;
+
+        this.frameX;
+        this.maxFrame = 5;
+        this.frameInterval = 100;
+        this.frameTimer = 0;
     }
+
     update(deltaTime) {
         this.x -= this.vx * deltaTime;
 
         if(this.x < 0 - this.width) {
             this.markedForDeletion = true;
         }
+
+        if(this.frameTimer > this.frameInterval) {
+            if(this.frameX < this.maxFrame) {
+                this.frameX++;
+            } else {
+                this.frameX = 0;
+            }
+            this.frameTimer = 0;
+        } else {
+            this.frameTimer += deltaTime;
+        }
+
     }
+
     draw(ctx) {
-        ctx.drawImage(this.image, 0, 0, this.spriteWidth, this.spriteHeigth, this.x, this.y, this.width, this.height)
+        ctx.drawImage(this.image, this.frameX * this.spriteWidth, 0, this.spriteWidth, this.spriteHeigth, this.x, this.y, this.width, this.height)
     }
 }
 
@@ -100,8 +123,18 @@ class Ghost extends Enemy {
 
         this.image = ghost;
         this.vx = Math.random() * 0.2 + 0.1;
+
+        this.angle = 0;
+        this.curve = Math.random() * 3;
     }
-    draw() {
+
+    update(deltaTime) {
+        super.update(deltaTime);
+        this.y += Math.sin(this.angle) * this.curve;
+        this.angle += 0.04;
+    }
+
+    draw(ctx) {
         ctx.save();
         ctx.globalAlpha = 0.5;
         super.draw(ctx)
@@ -109,17 +142,57 @@ class Ghost extends Enemy {
     }
 }
 
+class Spider extends Enemy {
+    constructor(game) {
+        super(game);
+
+        this.spriteWidth = 310;
+        this.spriteHeigth = 175;
+
+        this.width = this.spriteWidth / 2;
+        this.height = this.spriteHeigth / 2;
+
+        this.x = Math.random() * this.game.width;
+        this.y = 0 - this.height;
+
+        this.image = spider;
+        this.vx = 0;
+        this.vy = Math.random() * 0.1 + 0.1;
+        this.maxLength = Math.random() * game.height;
+    }
+
+    update(deltaTime) {
+        super.update(deltaTime);
+        if(this.y < 0 - this.height * 2) {
+            this.markedForDeletion = true;
+        }
+        this.y += this.vy * deltaTime;
+        if(this.y > this.maxLength) {
+            this.vy *= -1;
+        }
+    }
+
+    draw(ctx) {
+        ctx.beginPath();
+        ctx.moveTo(this.x + this.width / 2, 0);
+        ctx.lineTo(this.x + this.width / 2, this.y + 10);
+        ctx.stroke();
+        super.draw(ctx);
+    }
+}
 
 
 const game = new Game(ctx, canvas.width, canvas.height);
-let lastTime = 1;
+
+let lastTime = 0;
 function animate(timeStamp) {
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    ctx.clearRect(0, 0, canvas.width, canvas.height);  // clear canvas
 
     const deltaTime = timeStamp - lastTime;
     lastTime = timeStamp;
     game.update(deltaTime);
     game.draw();
+
     // some code
     requestAnimationFrame(animate);
 }
